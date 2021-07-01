7e8650cefb86e1ab5ea0c2de8e593ed24015b227
VolDemorT-acer <kunalsolanke1000@gmail.com>
2021-07-01 18:43:45 +0530

    fix import issue:

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const { setResToken } = require("../utils/tokens");
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
