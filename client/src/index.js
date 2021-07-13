4c16609655e8a3712e4d96c5f27b583e95faa746
VolDemorT-acer <kunalsolanke1000@gmail.com>
2021-07-13 20:08:02 +0530

    add sentry url to track exception

--- client/src/index.js
+++ client/src/index.js
@@ -9,16 +9,12 @@ import "./global.scss";
 import { Provider } from "react-redux";
 import store from "./store";
 
-// //Sentry Setup
-// Sentry.init({
-//     dsn: process.env.REACT_APP_SENTRY_URL,
-//     integrations: [new Integrations.BrowserTracing()],
-
-//     // Set tracesSampleRate to 1.0 to capture 100%
-//     // of transactions for performance monitoring.
-//     // We recommend adjusting this value in production
-//     tracesSampleRate: 1.0,
-// });
+//Sentry Setup
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_URL,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 if (process.env.REACT_APP_ENV != "dev") {
   window.console.log = () => {};
